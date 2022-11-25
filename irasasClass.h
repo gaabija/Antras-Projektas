306e2128b3264915a10e2049a15d8e20c2284531
gaabija <113300842+gaabija@users.noreply.github.com>
2022-11-25 15:09:35 +0200

    Update irasasClass.h

--- irasasClass.h
+++ irasasClass.h
@@ -20,24 +20,25 @@ private:
 public:
     Studentas() : egzas_(0) {}; // default konstruktorius
     Studentas(ifstream& fd);
-    string vardas() const { return vardas_;} // get'eriai, inline
-    void setVardas(string &v) { vardas_ = v; }
-    string pavarde() const { return pavarde_;}// get'eriai, inline
-    void setPavarde(string &p) { pavarde_ = p; }
+    string vardas() const { return vardas_;} // get'eriai
+    string pavarde() const { return pavarde_;}
     auto pazymiai() const { return pazymiai_;}
-    void setPazymiai(vector <int> &n) { pazymiai_ = n; }
     auto egzas() const { return egzas_;}
-    void setEgzas(int &eg) { egzas_ = eg; }
     auto galut() const { return galut_;}
+      
+    void setVardas(string &v) { vardas_ = v; }
+    void setPavarde(string &p) { pavarde_ = p; }
+    void setPazymiai(vector <int> &n) { pazymiai_ = n; }
+    void setEgzas(int &eg) { egzas_ = eg; }
     void setGalut(double &g) { galut_ = g; }
+      
     void Galutinis(char budasIsvesti);
-    void readStudentas(ifstream&); // set'eriai
     double Vidurkis();
     double Mediana();
     
-    ~Studentas() {}; //destruktorius
+    ~Studentas() {pazymiai_.clear();}; //destruktorius
 
-        Studentas(const Studentas &laikinas) // copy konstruktorius
+    Studentas(const Studentas &laikinas) // copy konstruktorius
    {
         vardas_ = laikinas.vardas_;
         pavarde_ = laikinas.pavarde_;
@@ -45,15 +46,13 @@ public:
         egzas_ = laikinas.egzas_;
     }
 
-    void swap(Studentas & kitas) {
-        ::swap(vardas_, kitas.vardas_);
-        ::swap(pavarde_, kitas.pavarde_);
-        ::swap(pazymiai_, kitas.pazymiai_);
-        ::swap(egzas_, kitas.egzas_);
-    }
     // copy assignment konstruktorius
-    Studentas & operator = (Studentas kitas){
-        swap(kitas);
+    Studentas & operator = (const Studentas &laikinas){
+        if (&laikinas == this) return *this;
+      vardas_ = laikinas.vardas_;
+        pavarde_ = laikinas.pavarde_;
+        pazymiai_ = laikinas.pazymiai_;
+        egzas_ = laikinas.egzas_;
         return *this;
     }
       
